fdedc1409159f9c60b44c3515f5764e18b8da24f
U-R\Rene Adaimi <rene.adaimi@gmail.com>
2016-09-24 18:24:08 -0400

    added gcd in c++

---
+++ C++/gcd.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+inline long long gcd(long long x, long long y) {
+	while (x > 0 && y > 0){
+		if (x > y){
+			x %= y;
+		} else {
+			y %= x;
+		}
+	}
+	return x + y;
+}
+
+int main(int argc, char const *argv[]){
+
+	return 0;
+}
