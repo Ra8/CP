6b085ef4200677f5135f03a99e8c634bb3449250
U-R\Rene Adaimi <rene.adaimi@gmail.com>
2016-09-24 17:04:14 -0400

    first commit with sieve

---
+++ C++/sieve.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int maxN=50000001;
+
+int primes[maxN];
+/*
+After calling sieve()
+For all i < maxN
+prime[i] = 1 iff i is prime
+prime[i] = 0 iff i is composite
+*/
+void sieve(){
+	for(int i=2;i<maxN;i++){
+		primes[i]=1;
+	}
+	for(long long i=2;i*i<maxN;i++){
+		if(primes[i]==1){
+			for(long long j=i*i;j<maxN;j+=i){
+				primes[j]=0;
+			}
+		}
+	}
+}
+
+int main(int argc, char const *argv[]){
+	sieve();
+	return 0;
+}
