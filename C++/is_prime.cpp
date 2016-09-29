fed8d864b917a0419950d65afa868d8cf73b375c
matthewdippel <mdippel@ccs.neu.edu>
2016-09-29 16:56:56 -0400

    minor efficieny change to c++ is_prime

--- C++/is_prime.cpp
+++ C++/is_prime.cpp
@@ -3,13 +3,15 @@
 using namespace std;
 bool isPrime(long long n){
 	if(n<2)return false;
-	for(long long i=2;i*i<=n;i++){
+	if(n % 2 == 0 || n % 3 == 0) return false;
+	for(long long i=5;i*i<=n;i += 6){
 		if(n%i==0)return false;
+		if(n % (i + 2) == 0) return false;
 	}
 	return true;
 }
 
 int main(int argc, char const *argv[]){
-
+    
 	return 0;
 }
