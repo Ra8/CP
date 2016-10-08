65570c10f5ebe902fd8338e653bb8923af426e2e
matthewdippel <mdippel@ccs.neu.edu>
2016-10-08 12:40:20 -0400

    better comments on python ccw.py

--- Python/geometry/ccw.py
+++ Python/geometry/ccw.py
@@ -1,3 +1,5 @@
+# Check clockwise / counter clockwise / colinearity of points
+
 def turn(p, q, r):
     result = (r[0] - q[0]) * (p[1] - q[1]) - (r[1] - q[1]) * (p[0] - q[0])
     if result < 0:
@@ -6,21 +8,20 @@ def turn(p, q, r):
     if result > 0:
         # P->Q->R is a left turn
         return 1
+    # P - Q - R are collinear 
     return 0
 
 
 def ccw(p, q, r):
+    """
+    Check if p, q, r make a counter clockwise turn
+    """
     return turn(p, q, r) > 0
 
 
 def intersect(a, b, c, d):
     """
     return if segment a-b intersects segment c-d
-    :param a:
-    :param b:
-    :param c:
-    :param d:
-    :return:
     """
     return (ccw(a, b, c) * ccw(a, b, d) <= 0) and (ccw(c, d, a) * ccw(c, d, b) <= 0)
 
