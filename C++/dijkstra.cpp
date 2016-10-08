b639e3e0c6d4f469e82f0206743d515e7583f57e
U-R\Rene Adaimi <rene.adaimi@gmail.com>
2016-10-08 01:11:27 -0400

    added dijkstra and floyd warshall

---
+++ C++/dijkstra.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long d[100010];
+int parent[100010]; // for parents
+long long inf = 100000000000000000;
+vector<pair<int, long long> > adj[100010];
+//adj[u][i] = {vertex, weight}
+int n; // number of verticies
+int m; // number of edges
+
+void dijkstra(int v){
+	fill(d, d+n+10, inf);
+	d[v] = 0;
+	int u;
+	set<pair<long long,int> > s;
+	s.insert({d[v], v});
+	while(!s.empty()) {
+		u = s.begin()->second;
+		s.erase(s.begin());
+		for(int i=0;i<adj[u].size();i++) {
+			pair<int, long long> p = adj[u][i];
+			if(d[p.first] > d[u] + p.second){
+				s.erase({d[p.first], p.first});
+				d[p.first] = d[u] + p.second;
+				s.insert({d[p.first], p.first});
+				parent[p.first]=u; // for parents
+			}
+		}
+	}
+}
+
+vector<int> getPath(int s, int t){
+	int u=t;
+	vector<int> path;
+	while(u!=s){
+		path.push_back(u);
+		u=parent[u];
+	}
+	path.push_back(s);
+	reverse(path.begin(), path.end());
+	return path;
+}
+
+int main(){
+	
+	return 0;
+}
---
+++ C++/floyd_warshall.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int SIZE=110; // maximum size of graph
+long double d[SIZE][SIZE];
+long double inf = 100000000000000000;
+int n; // actual size of graph
+
+void FloydWarshall(){
+	//check init below
+	for(int k=0;k<n;k++){
+		for(int i=0;i<n;i++){
+			for(int j=0;j<n;j++){
+				d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
+			}
+		}
+	}
+}
+void initFloydWarshall(){
+	//init for Floyd Warshall
+	for(int i=0;i<SIZE;i++){
+		for(int j=0;j<SIZE;j++){
+			d[i][j]=inf;
+			if(i==j){
+				d[i][j]=0;
+			}
+		}
+	}
+	//fill d[i][j] with actual weight
+}
+
+int main(int argc, char const *argv[]){
+	
+	return 0;
+}
