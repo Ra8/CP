18aeba8824fdd73a246971566c16889111f5e99e
U-R\Rene Adaimi <rene.adaimi@gmail.com>
2016-10-08 02:49:10 -0400

    Added file with useful functions in c++

---
+++ C++/_.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+	//scanf(\"%s %c %d %lf %I64d\",char*,&char,&int,&double,&long long);
+	cout << fixed << setprecision(12);
+	return 0;
+}
